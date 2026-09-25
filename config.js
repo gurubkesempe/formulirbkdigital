eb959a62a1e03e5b02c21e80ddc3101edbc577c6
gurubkesempe <pancehsr111@gmail.com>
2026-09-25 09:51:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@
    "https://script.google.com/macros/s/AKfycb.../exec"
    ========================================================= */
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzApuiVUEM9_GBHnM78XVmccvfa3PfCfDnu7hmp6Ly71cxEvIIvXX8NrFXYkGf6NAF2/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbwXLL8JUtnKTsa_Thqd_pBaco0yir44dDLv50RTFHE7bqxF6dPpl8GhuIB2AgPpjRt_/exec"
 };
